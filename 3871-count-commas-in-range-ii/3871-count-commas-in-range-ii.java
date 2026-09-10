6f0d6b6efa598e08ec838bff837d92522d39c979
Tanvi <tanvi.5singla@gmail.com>
2026-09-10 11:10:46 +0530

    Time: 1 ms (99.24%), Space: 42.7 MB (38.78%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -1,13 +1,8 @@
 class Solution {
     public long countCommas(long n) {
         long c=0;
-        long s=1000;
-        long cm=1;
-        while(s<=n){
-            long e=Math.min(n,s*1000-1);
-            c+=(e-s+1)*cm;
-            s*=1000;
-            cm++;
+        for(long i=1000;i<=n;i*=1000){
+            c+=n-i+1;
         }
         return c;
     }
