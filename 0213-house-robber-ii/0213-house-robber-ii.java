b1eb3bd9ae1eee9a9982b2bae6aa792da9a45a5b
Tanvi <tanvi.5singla@gmail.com>
2026-09-24 11:31:26 +0530

    Time: 0 ms (100%), Space: 43 MB (22.8%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,20 +1,20 @@
 class Solution {
     public int rob(int[] nums) {
+        if(nums.length==1) return nums[0];
         int n=nums.length;
-        if(n==1) return nums[0];
         int[] dp=new int[n];
         Arrays.fill(dp,-1);
-        int a=ans(nums,n-2,0,dp);
+        int a=solve(nums,dp,0,n-2);
         Arrays.fill(dp,-1);
-        int b=ans(nums,n-1,1,dp);
+        int b=solve(nums,dp,1,n-1);
         return Math.max(a,b);
     }
-    public int ans(int[] nums,int n,int start,int[] dp){
-        if(n<start) return 0;
+    public int solve(int[] nums,int[] dp,int start,int n){
+        if(n<start)return 0;
         if(n==start) return nums[n];
         if(dp[n]!=-1) return dp[n];
-        int p=nums[n]+ans(nums,n-2,start,dp);
-        int np=0+ans(nums,n-1,start,dp);
-        return dp[n]=Math.max(p,np);
+        int pick=nums[n]+solve(nums,dp,start,n-2);
+        int nonpick=0+solve(nums,dp,start,n-1);
+        return dp[n]=Math.max(pick,nonpick);
     }
 }
